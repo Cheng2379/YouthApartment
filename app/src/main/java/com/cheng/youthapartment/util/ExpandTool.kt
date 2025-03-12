c6d8db4bc1d2e7c392c8f46568a2a8b946cdbd23
“cheng” <2379367401@qq.com>
2025-03-13 00:54:59 +0800

    mix: 修复拓展函数的Parcelable参数错误导致的闪退

--- app/src/main/java/com/cheng/youthapartment/util/ExpandTool.kt
+++ app/src/main/java/com/cheng/youthapartment/util/ExpandTool.kt
@@ -2,11 +2,11 @@ package com.cheng.youthapartment.util
 
 import android.content.Intent
 import android.os.Build
+import android.os.Parcelable
 import android.view.View
 import android.widget.Toast
 import com.cheng.youthapartment.App
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.parcelize.Parcelize
 
 /**
  *
@@ -53,16 +53,16 @@ fun View.showSnackbar(
     snackbar.show()
 }
 
-@Suppress(
-    "FINAL_UPPER_BOUND",
-    "DEPRECATION",
-    "INFERRED_TYPE_VARIABLE_INTO_POSSIBLE_EMPTY_INTERSECTION"
-)
-inline fun <reified T : Parcelize> Intent.getYAParcelableExtra(
+@Suppress("DEPRECATION")
+inline fun <reified T : Parcelable> Intent.getYAParcelableExtra(
     key: String
-): T? = when {
-    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
-    else -> getParcelableExtra(key) as? T
+): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key) as? T
+    }
 }
 
 
